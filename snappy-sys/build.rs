be02178330bb17648d6ac605af249eba18b32b71
Jay Lee <busyjaylee@gmail.com>
2017-11-30 13:34:45 +0800

    fix windows support

--- snappy-sys/build.rs
+++ snappy-sys/build.rs
@@ -9,11 +9,22 @@ use cmake::Config;
 fn main() {
     let src = env::current_dir().unwrap().join("snappy");
     let dst = Config::new("snappy").build_target("snappy").build();
-    let build = dst.join("build");
+    let mut build = dst.join("build");
+    let stub = build.join("snappy-stubs-public.h");
+    if cfg!(target_os = "windows") {
+        let profile = match &*env::var("PROFILE").unwrap_or("debug".to_owned()) {
+            "bench" | "release" => "Release",
+            _ => "Debug",
+        };
+        build = build.join(profile);
+    }
     println!("cargo:root={}", build.display());
     println!("cargo:rustc-link-lib=static=snappy");
     println!("cargo:rustc-link-search=native={}", build.display());
     fs::copy(src.join("snappy.h"), build.join("snappy.h")).unwrap();
+    if cfg!(target_os = "windows") {
+        fs::copy(stub, build.join("snappy-stubs-public.h")).unwrap();
+    }
     configure_stdcpp();
 }
 
