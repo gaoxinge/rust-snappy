58095afbaa3a4c8442158137c7d3c8347aecc5ce
Ana Hobden <operator@hoverbear.org>
2019-06-06 11:09:40 -0700

    Simplify
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- snappy-sys/build.rs
+++ snappy-sys/build.rs
@@ -3,60 +3,28 @@ extern crate cmake;
 
 use std::env;
 use std::fs;
-use std::ffi::OsString;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+
+use cmake::Config;
 
 fn main() {
-    let want_static = env::var("SNAPPY_SYS_STATIC").unwrap_or(String::new()) == "1";
-    let from_source = env::var("SNAPPY_SYS_STATIC_FROM_SOURCE").unwrap_or(String::new()) == "1";
-    if !from_source && configure_snappy(want_static) {
-        return;
+    let src = env::current_dir().unwrap().join("snappy");
+    let dst = Config::new("snappy").build_target("snappy").build();
+    let mut build = dst.join("build");
+    let stub = build.join("snappy-stubs-public.h");
+    if cfg!(target_os = "windows") {
+        let profile = match &*env::var("PROFILE").unwrap_or("debug".to_owned()) {
+            "bench" | "release" => "Release",
+            _ => "Debug",
+        };
+        build = build.join(profile);
     }
-    build_snappy();
-}
-
-fn configure_snappy(want_static: bool) -> bool {
-    // ~ try pkg_config first
-    if pkg_config::probe_library("snappy").is_ok() {
-        return true;
-    }
-    // ~ then try search in statically predefined directories
-    let libsnappy_file = if want_static { "libsnappy.a" } else { "libsnappy.so" };
-    if let Some(path) = first_path_with_file(libsnappy_file) {
-        if want_static {
-            println!("cargo:rustc-link-search={}", path);
-            println!("cargo:rustc-link-lib=static=snappy");
-            configure_stdcpp();
-        } else {
-            println!("cargo:rustc-link-search=native={}", path);
-            println!("cargo:rustc-link-lib=dylib=snappy");
-        }
-        return true;
-    }
-    return false;
-}
-
-fn build_snappy() {
-    let out_dir = PathBuf::from(&env::var("OUT_DIR").unwrap());
-
-    let cc = cc::Build::new().cpp(true).get_compiler();
-    let mut cflags = OsString::new();
-    for arg in cc.args() {
-        cflags.push(arg);
-        cflags.push(" ");
-    }
-    let output = cmake::Config::new("snappy")
-        .env("CC", cc.path())
-        .env("CFLAGS", cflags)
-        .static_crt(true)
-        .build();
-    Command::new("make").current_dir(&output).status().unwrap();
+    println!("cargo:root={}", build.display());
     println!("cargo:rustc-link-lib=static=snappy");
-    // On some machines, we see lib, on other machines, we see lib64. Add both:
-    println!("cargo:rustc-link-search=native={}/lib", output.display());
-    println!("cargo:rustc-link-search=native={}/lib64", output.display());
-    println!("cargo:root={}", out_dir.to_string_lossy());
+    println!("cargo:rustc-link-search=native={}", build.display());
+    fs::copy(src.join("snappy.h"), build.join("snappy.h")).unwrap();
+    if cfg!(target_os = "windows") {
+        fs::copy(stub, build.join("snappy-stubs-public.h")).unwrap();
+    }
     configure_stdcpp();
 }
 
@@ -72,30 +40,5 @@ fn configure_stdcpp() {
     };
     if let Some(cpp) = cpp {
         println!("cargo:rustc-link-lib={}", cpp);
-    }
-}
-
-fn first_path_with_file(file: &str) -> Option<String> {
-    // we want to look in LD_LIBRARY_PATH and then some default folders
-    if let Some(ld_path) = env::var_os("LD_LIBRARY_PATH") {
-        for p in env::split_paths(&ld_path) {
-            if is_file_in(file, &p) {
-                return p.to_str().map(|s| String::from(s))
-            }
-        }
-    }
-    for p in vec!["/usr/lib","/usr/local/lib"] {
-        if is_file_in(file, &Path::new(p)) {
-            return Some(String::from(p))
-        }
-    }
-    return None
-}
-
-fn is_file_in(file: &str, folder: &Path) -> bool {
-    let full = folder.join(file);
-    match fs::metadata(full) {
-        Ok(ref found) if found.is_file() => true,
-        _ => false
     }
 }
